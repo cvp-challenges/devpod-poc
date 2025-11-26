ecb04616d7e41c74f7a8d11f4662e6b100754089
Jason Whitehead <jasonwhitehead@cvpcorp.com>
2025-11-26 11:39:40 -0600

    changes

--- .devcontainer/scripts/entrypoint.sh
+++ .devcontainer/scripts/entrypoint.sh
@@ -6,14 +6,8 @@ INIT_FLAG="/workspace/.initialized"
 if [ ! -f "$INIT_FLAG" ]; then
   echo "🚀 First-time initialization..."
 
-  ls -al /workspace/.devcontainer/scripts
-
-  chmod +x /workspace/.devcontainer/scripts/setup-env-vars.sh
-
-  ls -al /workspace/.devcontainer/scripts
-
   # Generate .env file
-  /workspace/.devcontainer/scripts/setup-env-vars.sh /workspace
+  bash /workspace/.devcontainer/scripts/setup-env-vars.sh /workspace
 
   echo "📚 Cloning repositories..."
 
